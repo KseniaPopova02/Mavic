b323b82936a5c7e073c076f2d5746c1ab2025aac
Ksenia Popova <98548104+KseniaPopova02@users.noreply.github.com>
2022-10-11 10:57:44 +0200

    fixed images

--- webpack.mix.js
+++ webpack.mix.js
@@ -1,5 +1,7 @@
 let mix = require("laravel-mix");
 
-mix.sass("css/style.scss", "css/style.css");
+mix.sass("css/style.scss", "css/style.css").options({
+  processCssUrls: false,
+});
 
 mix.js("js/main.js", "js/build.js");
